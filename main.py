3bda29b5ad88bcb94811cbce0f468826e73f8053
slink007 <stephen.link0@gmail.com>
2025-10-09 14:07:42 -0400

    Make program take question prompt from command line.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,12 +9,16 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', 
-        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    try:
+        prompt = sys.argv[1]
+        response = client.models.generate_content(model='gemini-2.0-flash-001', contents=prompt)
+        print(response.text)
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    except IndexError:
+        print("Error: no prompt provided")
+        exit(1)
 
 if __name__ == "__main__":
     main()
