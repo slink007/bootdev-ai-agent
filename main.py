a63b3eaa2b9dfcc9d209375017ded263912248d6
slink007 <stephen.link0@gmail.com>
2025-10-13 16:13:50 -0400

    Give script ability to use '--verbose' to control level of output.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import sys
 
+
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -10,15 +12,26 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     
+    prompt = ""
+    response = ""
+    
     try:
         prompt = sys.argv[1]
-        response = client.models.generate_content(model='gemini-2.0-flash-001', contents=prompt)
-        print(response.text)
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        messages = [types.Content(role="user", parts=[types.Part(text=prompt)]),]
+        response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
     except IndexError:
         print("Error: no prompt provided")
         exit(1)
+    
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+
 
 if __name__ == "__main__":
     main()
+
+
+
